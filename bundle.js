14941fce188b46d53f1af3564f4b8295c97eb556
sjf125 <sjf125@gmail.com>
2016-09-23 17:17:58 -0400

    Grunt build

--- bundle.js
+++ bundle.js
@@ -75,7 +75,8 @@ webpackJsonp([0],[
 	'use strict';
 
 	var app = {
-	  api: 'http://localhost:3000'
+	  // api: 'http://localhost:3000'
+	  api: 'https://fp-server.herokuapp.com/'
 	};
 
 	module.exports = app;
